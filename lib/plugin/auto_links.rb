7e02cf22536726a873fdf358bf4ca05053ffc996
blindgaenger <blindgaenger@gmail.com>
2008-12-13 01:16:20 +0100

    plugin for automatically convert http links

---
+++ lib/plugin/auto_links.rb
@@ -0,0 +1,9 @@
+# be careful not to harm the [[wikilinks]] plugin
+# so the link need to start with BOL or a whitespace
+module BlikiContent
+  def plugin_auto_links content
+    content.gsub(/(^|\s)(http\:\/\/\S+)/) do |m|
+      "#{$1}<a href=\"#{$2}\">#{$2}</a>"
+    end
+  end
+end
