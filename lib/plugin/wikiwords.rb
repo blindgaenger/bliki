355a3ae8a12e0434d2bf054b3736537acffbda7c
blindgaenger <blindgaenger@gmail.com>
2008-12-18 22:45:16 +0100

    commented wikiwords as they break the syntax highlight plugin

--- lib/plugin/wikiwords.rb
+++ lib/plugin/wikiwords.rb
@@ -1,7 +1,7 @@
-module BlikiContent
-  def plugin_wikiwords content
-    content.gsub(/([A-Z]+)([a-z]+)([A-Z]+)\w+/) do |m|
-      " <a href=\"#{Sinatra.options.base_url}/#{m.strip.downcase}\">#{m.strip}</a> "
-    end
-  end
-end
+#module BlikiContent
+#  def plugin_wikiwords content
+#    content.gsub(/([A-Z]+)([a-z]+)([A-Z]+)\w+/) do |m|
+#      " <a href=\"#{Sinatra.options.base_url}/#{m.strip.downcase}\">#{m.strip}</a> "
+#    end
+#  end
+#end
