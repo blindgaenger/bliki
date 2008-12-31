54a1f4eccd98dbc41fb530acf53119b3388cb75f
blindgaenger <blindgaenger@gmail.com>
2008-12-31 18:40:54 +0100

    all static files, but the stylesheet, are in public now

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -3,7 +3,3 @@ get '/base.css' do
   cache(sass(:base))
 end
 
-# Theme support
-get '/:type/:filename.:ext' do
-  send_file "app/views/#{params[:type]}/#{params[:filename]}.#{params[:ext]}", :disposition => "inline"
-end
