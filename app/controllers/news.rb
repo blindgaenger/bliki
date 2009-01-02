f4acee2a94b7f4358a9e069adb6efa595edddd75
blindgaenger <blindgaenger@gmail.com>
2009-01-02 15:23:32 +0100

    reimplemented disqus support

--- app/controllers/news.rb
+++ app/controllers/news.rb
@@ -1,15 +1,5 @@
 get '/' do
-#  @pages = Page.latest(Sinatra.options.limit)
-
-  @pages = Page.all(:order => {:updated_at => :desc}).first(@app.limit)
-
-#  all_pages = Page.all(:order => {:created_at => :desc})
-#  @pages = all_pages.first(Sinatra.options.limit)
-
-#  if all_pages.size > Sinatra.options.limit
-#    @archives = all_pages[(Sinatra.options.limit)...Sinatra.options.limit*2]
-#  end
-
+  @pages = Page.latest(@app.limit)
   erb :news
 end
 
---
+++ app/helpers/disqus_helpers.rb
@@ -0,0 +1,53 @@
+def disqus_head
+  if @app.use_comments && !production?
+    <<-XXX
+<script type="text/javascript">
+//<![CDATA[
+  var disqus_developer = 1;
+//]]>
+</script>
+    XXX
+  end
+end
+
+def disqus_comments
+  if @app.use_comments
+    <<-XXX
+<div id="disqus_thread"></div><script type="text/javascript" src="http://disqus.com/forums/#{@app.disqus_id}/embed.js"></script><noscript><a href="http://#{@app.disqus_id}.disqus.com/?url=ref">View the discussion thread.</a></noscript>
+    XXX
+  end
+end
+
+
+def disqus_scripts
+  if @app.use_comments 
+    <<-XXX
+<script type="text/javascript">
+//<![CDATA[
+(function() {
+    var links = document.getElementsByTagName('a');
+    var query = '?';
+    for(var i = 0; i < links.length; i++) {
+	    if(links[i].href.indexOf('#disqus_thread') >= 0) {
+		    query += 'url' + i + '=' + encodeURIComponent(links[i].href) + '&';
+	    }
+    }
+    document.write('<script type="text/javascript" src="http://disqus.com/forums/#{@app.disqus_id}/get_num_replies.js' + query + '"></' + 'script>');
+  })();
+//]]>
+</script>
+    XXX
+  end
+end
+
+def disqus_link(page, text)
+  if @app.use_comments
+    <<-XXX
+<div>#{link_to text, url_for_pages_comments(page)}</div>
+    XXX
+  end
+end
+
+def url_for_pages_comments(page)
+  "#{url_for_pages_show(page)}#disqus_thread"
+end
